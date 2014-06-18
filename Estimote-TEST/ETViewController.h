3ee81ba1be847e322405ab21ca05b738c24b3c7f
Anditto Heristyo <anditto.heristyo@gmail.com>
2014-06-18 17:53:14 +0900

    Initial Commit

---
+++ Estimote-TEST/ETViewController.h
@@ -0,0 +1,13 @@
+//
+//  ETViewController.h
+//  Estimote-TEST
+//
+//  Created by アンディット ヘリスティヨ on 2014/06/18.
+//  Copyright (c) 2014年 Digital Garage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ETViewController : UIViewController
+
+@end
---
+++ Estimote-TEST/ETViewController.m
@@ -0,0 +1,29 @@
+//
+//  ETViewController.m
+//  Estimote-TEST
+//
+//  Created by アンディット ヘリスティヨ on 2014/06/18.
+//  Copyright (c) 2014年 Digital Garage. All rights reserved.
+//
+
+#import "ETViewController.h"
+
+@interface ETViewController ()
+
+@end
+
+@implementation ETViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
